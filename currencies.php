7cbdaea0ced4b980db6eb9c1ef335ebcf42d9c20
suraj muhammad <soorajwizard01@gmail.com>
2023-07-18 17:10:51 +0300

    added all the files, the rest API is working as required for initial launch

--- currencies.php
+++ currencies.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
 require "./sniper.php";
 
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
